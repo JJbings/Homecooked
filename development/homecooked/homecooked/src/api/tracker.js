ed4489eb92fed38906456a22e1083bbb668341b9
Joco Bogdanovic <96065047+bgdnvc@users.noreply.github.com>
2023-05-15 17:58:35 +0200

    added refined c4

--- development/homecooked/homecooked/src/api/tracker.js
+++ development/homecooked/homecooked/src/api/tracker.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL:
-		"https://9c73-2001-1c05-1f31-7f00-8ddc-6aa0-6fbc-96ae.ngrok-free.app",
+	baseURL: "https://fe97-145-93-148-20.ngrok-free.app",
 });
